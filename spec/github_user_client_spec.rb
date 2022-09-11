f3f820f380f43b865438bdae2db3a977bf4f3f1a
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-11 12:51:12 -0600

    Add tests for user_client

--- spec/github_user_client_spec.rb
+++ spec/github_user_client_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe GithubUsersClient do
     expect(user_client).to be_instance_of(GithubUsersClient)
   end
 
+  it "can parse data into a hash" do
+    user_client = GithubUsersClient.new
+
+    expect(user_client.get("Alaina-Noel").class).to eq(Hash)
+  end
+
 end
